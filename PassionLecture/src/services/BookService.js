206125d991fc0870c0dc84fcc4251ba597afb851
latifcodess <196327957+LatifKrasniqi24@users.noreply.github.com>
2026-02-11 11:09:05 +0100

    chore(services): ajout de l'appel du serveur json

---
+++ PassionLecture/src/services/BookService.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+const apiClient = axios.create({
+  baseURL: 'https://my-json-server.typicode.com/AsDeLaGachette/PassionLecture_294',
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
+})
+
+export default {
+    
+}
