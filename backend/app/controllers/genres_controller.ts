26dfedf6fc2771b81e03b2174c23b44d9a905065
latifcodess <latif.krasniqi@eduvaud.ch>
2026-05-12 17:24:18 +0200

    trop2truc: j'ai ajouté les test bruno et les methodes des controlleurs manquantes
    [120][Done]

--- backend/app/controllers/genres_controller.ts
+++ backend/app/controllers/genres_controller.ts
@@ -9,9 +9,45 @@ export default class GenresController {
     const response = await Genre.all()
     return response
   }
+  
+  /**
+   * Handle form submission for the create action
+   */
+  async store({ request, response }: HttpContext) {
+    const data = request.all()
+
+    const genre = await Genre.create(data)
+
+    return response.created(genre)
+  }
 
   /**
    * Show individual record
    */
-  async show({ params }: HttpContext) {}
+  async show({ params }: HttpContext) {
+    const genre = await Genre.query()
+          .where('id', params.id)
+          .firstOrFail()
+        return genre
+  }
+
+   /**
+   * Handle form submission for the edit action
+   */
+  async update({ params, request }: HttpContext) {
+    const data = request.all()
+    const genre = await Genre.findOrFail(params.id)
+    genre.merge(data)
+    return genre.save()
+  }
+
+  /**
+     * Delete record
+     */
+    async destroy({ params, response }: HttpContext) {
+      const genre = await Genre.query().where('id', params.id).firstOrFail()
+  
+      await genre.delete()
+      return response.noContent()
+    }
 }
