81333e453cd6df111e9e93e6d79b653e243052bb
David Sottas <david.sottas@eduvaud.ch>
2026-05-26 17:18:15 +0200

    fix: fixed merge issue
    [29][DONE]

--- backend/app/validators/review.ts
+++ backend/app/validators/review.ts
@@ -4,10 +4,6 @@ const ReviewValidator = vine.compile(
     title: vine.string().minLength(2).maxLength(255),
     rating: vine.number().min(1).max(5),
     comment: vine.string().minLength(2).maxLength(255),
-    userId: vine.number().exists(async (db, value) => {
-      const user = await db.from('users').where('id', value).first()
-      return !!user
-    }),
   })
 )
 export { ReviewValidator }
